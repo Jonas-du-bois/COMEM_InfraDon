92593c157a87383b719ef9ad53796db37f878240
Jonas Du Bois <jonas.du.bois@outlook.com>
2024-09-26 13:35:28 +0200

    repository github pour le cours InfraDon2

---
+++ infradon2/src/views/NoSQLView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="example-component">
+    <h1>{{ message }}</h1>
+    <button @click="incrementNumber">Cliquez-moi</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ExampleComponent',
+  data() {
+    return {
+      number: 0
+    };
+  },
+  methods: {
+    incrementNumber() {
+      this.number++;
+    }
+  },
+  computed: {
+    message() {
+      return `Le numéro est : ${this.number}`;
+    }
+  }
+};
+</script>
+
+<style scoped>
+.example-component {
+  text-align: center;
+  margin-top: 20px;
+}
+
+button {
+  padding: 10px 20px;
+  font-size: 16px;
+  cursor: pointer;
+}
+</style>
